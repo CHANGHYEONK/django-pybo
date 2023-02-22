3667804b2d96f1a038b7c7ec5fa667656f399a09
CH_Kim <bokk373@gmail.com>
2023-02-22 17:39:41 +0900

    장고 2장 진도 끝

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 # def index(request):
@@ -39,5 +40,34 @@ def answer_create(request, question_id):
     # content = request.POST.get('content', '') # 2) 키가 없으면 None 반환 or ,뒤에 있는 값 반환
     # print('get(content)', content)
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    '''
+    pybo 질문등록
+    '''
+    if request.method == 'POST':    # submit을 통한 POST 요청
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:                           # GET 요청
+        form = QuestionForm()
+    
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
